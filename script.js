194e157e351e76c6e5d961159a0db32ec9063bb3
crossSiteKikyo <patmailaddress01@gmail.com>
2024-12-02 16:04:24 +0900

    최근방식 iframe삽입

--- script.js
+++ script.js
@@ -11,13 +11,21 @@ function analyseURL() {
 
     // 중간에 -가 들어가 있다면 볼 수 있는 영상.
     const imageContent = document.querySelector('meta[property="og:image"]').content;
+    console.log(`imageContent: ${imageContent}`);
     const mp4Name = imageContent.match(/[^/]+.mp4/)[0];
+    console.log(`mp4Name: ${mp4Name}`);
     // __NUXT_DATA__에서 mp4Name가 들어있다. 5개월 이상 된 옛날 방식인지 최근 방식인지 확인한다.
 	const searchFromObfuscatedRegex = new RegExp(`"https[\\s\\S]+?${mp4Name}"`, 'g');
-    const result = document.querySelector('#__NUXT_DATA__').innerHTML.match(searchFromObfuscatedRegex);
+    console.log(`searchFromObfuscatedRegex: ${searchFromObfuscatedRegex}`);
+    const result = document.querySelector('#__NUXT_DATA__').innerHTML.match(searchFromObfuscatedRegex)[0];
+    console.log(`result: ${result}`);
     // 결과가 있다면 최근방식.
     if(result) {
         alert("최근방식");
+        const videoSrc = result.replace(/[",]/g, "");
+        console.log(videoSrc);
+        //미리보기와 버튼 삭제후 video 태그 삽입.
+        document.querySelector('.player-wrapper.relative.bg-base-200').outerHTML=`<video src="${videoSrc}" style="width:100%;height:100%;" data-v-02e5f75f="" controls></video>`;
     }
     // 결과가 없다면 옛날방식.
     else {
