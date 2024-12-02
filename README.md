4ce938200d6b889681b29a8e09291650b2268215
crossSiteKikyo <patmailaddress01@gmail.com>
2024-12-02 15:20:12 +0900

    영상 유형 분석까지

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # skbjBypassVip
+skbj에서 vip가 아니거나 코인이 없어서 볼 수 없는 영상을 볼 수 있게 만드는 스크립트
+
+## 사용법
+### 북마크 사용법
+1. 아래 스크립트를 복사하기
+```javascript:(function(){fetch('https://raw.githubusercontent.com/crossSiteKikyo/skbjBypassVip/refs/heads/main/script.js').then((res)=>res.text()).then(scriptContent=>{const skbjScript=document.createElement('script');skbjScript.textContent=scriptContent;document.head.appendChild(skbjScript);})})();```
+2. 브라우저 북마크 또는 즐켜찾기에 추가하기. 이름은 skbj스크립트, url에 위 복사한것 붙여넣기
+3. 볼 수 없는 영상에서 북마크 클릭하기
--- script.js
+++ script.js
@@ -1,5 +1,24 @@
 function skbjBypassVip() {
-    alert(1);
+    analyseURL();
+}
+function analyseURL() {
+    /* https://skbj.tv/videos/l2npo1xri1sr와 같이 영문숫자로만 되어있다면 볼 수 없다. 
+    현재 해킹당하거나 영상이 내려간 것으로 보임. */
+    if(/^https:\/\/skbj.tv\/videos\/[0-9a-zA-Z]+$/.test(window.location.href)) {
+        alert("해당 영상은 볼 수 없습니다");
+        return;
+    }
+
+    // 중간에 -가 들어가 있다면 볼 수 있는 영상.
+    const imageContent = document.querySelector('meta[property="og:image"]').content;
+    const mp4Name = imageContent.match(/[^/]+.mp4/)[0];
+    // __NUXT_DATA__에서 mp4Name가 들어있다. 5개월 이상 된 옛날 방식인지 최근 방식인지 확인한다.
+	const searchFromObfuscatedRegex = new RegExp(`"https[\s\S]+?${mp4Name}"`, 'g');
+    const result = document.querySelector('#__NUXT_DATA__').innerHTML.match(searchFromObfuscatedRegex);
+    // 결과가 없다면 옛날방식.
+    alert("옛날방식");
+    // 결과가 있다면 최근방식.
+    alert("최근방식");
 }
 
 skbjBypassVip();
